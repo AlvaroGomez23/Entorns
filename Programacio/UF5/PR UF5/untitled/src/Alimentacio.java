78f53033842fcaed6cb9eb9f1e5b58ecac9a2440
Alvaro <a.gomez9@sapalomera.cat>
2024-04-12 12:37:19 +0200

    estructurant porjecte

--- Programacio/UF5/PR UF5/untitled/src/Alimentacio.java
+++ Programacio/UF5/PR UF5/untitled/src/Alimentacio.java
@@ -1,9 +1,17 @@
+import java.util.Calendar;
+import java.util.Date;
+
 public class Alimentacio extends Producte{
 
-    String dataCaducitat;
+    Date dataCaducitat;
 
-    public Alimentacio(float preu, String nom, String codiBarres, String dataCaducitat) {
+    public Alimentacio(float preu, String nom, String codiBarres, Date dataCaducitat) {
         super(preu, nom, codiBarres);
         this.dataCaducitat = dataCaducitat;
+    }
+
+    public void setPreu() {
+        Calendar c = Calendar.getInstance();
+        //preu = (float) (preu-preu*(1/(dataCaducitat-dataActual+1)) + (preu * 0.1));
     }
 }
--- Programacio/UF5/PR UF5/untitled/src/Main.java
+++ Programacio/UF5/PR UF5/untitled/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
 
         int hola;
 
         System.out.println("Print hola");
+
+        Calendar c = Calendar.getInstance();
+
+        System.out.println(Calendar.DAY_OF_YEAR);
 
 
     }
