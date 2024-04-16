25524fc6fd14b2e1f9ed89219bbe42b5cf78434b
Alvaro <a.gomez9@sapalomera.cat>
2024-04-16 13:08:53 +0200

    Classe afegir alimentació actualitzada

--- Programacio/UF5/PR UF5/untitled/src/Main.java
+++ Programacio/UF5/PR UF5/untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,22 +16,26 @@ public class Main {
         switch (opcio) {
             case 1:
                 int op2 = 0;
+                System.out.println("-------------");
+                System.out.println("PRODUCTE");
+                System.out.println("-------------");
                 System.out.println("Quin tipus de producte vols?");
                 System.out.println("1. Alimentacio");
                 System.out.println("2. Tèxtil");
                 System.out.println("3. Electrònica");
                 System.out.println("0. Tornar ");
                 op2 = scan.nextInt();
+                scan.nextLine();
 
                 switch (op2) {
                     case 1:
-                        //Crida classe alimentació
+                        afegirAlimentacio();
                         break;
                     case 2:
-                        //Crida classe tèxtil
+                        afegirTextil();
                         break;
                     case 3:
-                        //Crida classe electrònica
+                        afegirElectronica();
                         break;
                     default:
                         //Sortir del programa
@@ -45,4 +51,53 @@ public class Main {
 
 
     }
+
+    static void afegirAlimentacio() {
+
+        try {
+            String nom = "Guabaya";
+            float preu = 0;
+            String codiBarres = "0";
+            //Date dataCaducitat = "0";
+            System.out.println("Afegir alimentacio");
+            System.out.print("Nom: ");
+            nom = scan.nextLine();
+
+            System.out.print("Preu: ");
+            preu = scan.nextInt();
+            scan.nextLine();
+
+            System.out.print("Codi de barres: ");
+            codiBarres = scan.nextLine();
+
+            System.out.print("Data de caducitat: ");
+            System.out.println();
+            //dataCaducitat = scan.nextLine();
+            Alimentacio aliment = new Alimentacio(preu, nom, codiBarres, null);
+        } catch (InputMismatchException e){
+            System.out.println("Hi ha hagut un problema al afegir el aliment (Dades introduides malament)");
+            System.out.println("Torna a introduir el producte");
+            afegirAlimentacio();
+        } catch (Exception e) {
+            System.out.println("Hi ha hagut un problema al afegir l'aliment"+e.getCause());
+            System.out.println("Torna a introduir el producte");
+            afegirAlimentacio();
+        }
+    }
+
+    static void afegirTextil() {
+        String nom;
+        float preu;
+        String composicio;
+        String codiBarres;
+    }
+
+    static void afegirElectronica() {
+        String nom;
+        float preu;
+        int garantia;
+        String codiBarres;
+    }
+
+    
 }
